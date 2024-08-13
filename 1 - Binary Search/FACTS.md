51ad7430f6a2643606eea8c2d19b2470e82a6a6a
Owen Mariani <mariani.owen@gmail.com>
2024-08-13 13:32:50 -0700

    binary search complete

--- 1 - Binary Search/FACTS.md
+++ 1 - Binary Search/FACTS.md
@@ -1,8 +1,11 @@
 # Algorithm Facts
-quick description
+Given a sorted array return the index of an element in the array, otherwise return -1
 
 ## Purpose
+Find index of an element in a list
 
 ## Time Complexity
+O(logn) - divide the array by 2 every iteration
 
 ## Space Complexity
+O(1) - no array is copied, just sliced. return value is O(1)
---
+++ 1 - Binary Search/binarySearch.py
@@ -0,0 +1,62 @@
+# iterative approach
+def iterativeBinarySearch(sortedArr, elem):
+    if not sortedArr or not elem:
+        raise ValueError("Inputs are undefined")
+    if len(sortedArr) == 0:
+        return -1
+    
+    front = 0
+    end = len(sortedArr) -1
+
+    # once they switch that means you've checked every relevant index
+    while front <= end:
+        middle = front + (end - front) // 2
+        if sortedArr[middle] == elem:
+            return middle
+        elif elem < sortedArr[middle]:
+            end = middle - 1
+        else:
+            front = middle + 1
+    
+    return -1
+
+def iterativeCheck():
+    # print(iterativeBinarySearch(array1, 8))
+    # print(iterativeBinarySearch(array2, 45))
+    # print(iterativeBinarySearch(array3, -22))
+    assert iterativeBinarySearch(array1, 8) == 1
+    assert iterativeBinarySearch(array2, 45) == len(array2)-1
+    assert iterativeBinarySearch(array3, -22) == -1
+    print('[ITERATIVE] All tests passed!')
+
+def recursiveBinarySearch(sortedArr, elem):
+    
+    # base case
+    if len(sortedArr) == 0:
+        return -1
+    
+    # recursive step
+    middle = len(sortedArr) // 2
+    if sortedArr[middle] == elem:
+        return middle
+    else:
+        if sortedArr[middle] > elem:
+            return recursiveBinarySearch(sortedArr[:middle], elem)
+        else:
+            return middle + recursiveBinarySearch(sortedArr[middle:], elem)
+
+def recursiveCheck():
+    # print(recursiveBinarySearch(array1, 8))
+    # print(recursiveBinarySearch(array2, 45))
+    # print(recursiveBinarySearch(array3, -22))
+    assert recursiveBinarySearch(array1, 8) == 1
+    assert recursiveBinarySearch(array2, 45) == len(array2)-1
+    assert recursiveBinarySearch(array3, -22) == -1
+    print('[RECURSIVE] All tests passed!')
+
+if __name__ == "__main__":
+    array1 = [3, 8, 15, 23, 42, 56, 77] # odd num of elements
+    array2 = [1, 5, 9, 12, 19, 23, 34, 45] # even num of elements
+    array3 = [-21, -15, -7, 0, 4, 11, 18, 27, 33] # positives and negatives
+    iterativeCheck()
+    recursiveCheck()
